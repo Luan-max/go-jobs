ff2746080332d8ea5667adfd94aa191a6520c7cd
Luan-max <luannlopessilva@outlook.com>
2023-05-21 21:13:13 -0300

    feat: add handler and swagger setup

--- router/routes.go
+++ router/routes.go
@@ -1,18 +1,23 @@
 package router
 
 import (
+	docs "github.com/Luan-max/go-jobs/docs"
 	"github.com/Luan-max/go-jobs/handler"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	handler.InitHandler()
-
+	docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/jobs", handler.CreateJobHandler)
 		v1.DELETE("/jobs", handler.DeleteJobHandler)
 	}
+
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
